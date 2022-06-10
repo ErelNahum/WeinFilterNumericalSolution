f9858347c253268b14f6e29f18f321f8b564961f
ErelNahum <erelnahum2003@gmail.com>
2022-06-10 15:50:17 +0300

    coded q6 added matching graphs

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import math
+import scipy.constants as s
 
 
 # first order taylor
@@ -14,3 +15,13 @@ T = 2 * math.pi / w
 analytic_ry_T = 0
 analytic_rz_T = E / B * T
 analytic_T = analytic_ry_T, analytic_rz_T
+
+class q6:
+    m = s.m_p
+    q = s.e
+    B = 0.5
+    w = q * B / m
+    T = 2 * math.pi / w
+    MEV_TO_J = 1.60217663e-13
+    E0 = 5
+    R = 3e-3
---
+++ q6.py
@@ -0,0 +1,57 @@
+import numpy as np
+import constants as c
+import math
+import matplotlib.pyplot as plt
+
+
+def calculate_v0(E, m):
+    E *= c.q6.MEV_TO_J
+    return math.sqrt(2 * E / m)
+
+
+def ay(vz, E):
+    return c.q6.q / c.q6.m * (E - c.q6.B * vz)
+
+
+def az(vy):
+    return c.q6.q / c.q6.m * c.q6.B * vy
+
+
+def q6_graph(intervals, Ei, y0, draw=False):
+    v0 = calculate_v0(c.q6.E0, c.q6.m)
+    E = v0 * c.q6.B
+    v0 = calculate_v0(Ei, c.q6.m)
+    dt = c.q6.T / (intervals - 1)
+
+    vy = [0]
+    vz = [v0]
+    ry = [y0]
+    rz = [0]
+
+    for t in range(intervals - 1):
+        vy.append(vy[t] + ay(vz[t], E) * dt)
+        vz.append(vz[t] + az(vy[t]) * dt)
+        ry.append(ry[t] + vy[t] * dt)
+        rz.append(rz[t] + vz[t] * dt)
+
+        if abs(ry[t + 1]) > c.q6.R:
+            break
+
+    if draw:
+        plt.suptitle(r"$(r_y, r_z)$ : route of a proton with $y_0=-1.5\cdot10^{-4}$ $E_i=4.98\;MeV$")
+        plt.grid()
+        plt.plot(ry, rz, color="red")
+        plt.axvline(x=c.q6.R)
+        plt.axvline(x=-c.q6.R)
+
+
+        # axis[1].set_title(r"$(v_y, v_z)$")
+        # axis[1].grid()
+        # axis[1].plot(vy, vz)
+        plt.show()
+    return ry[-1], rz[-1]
+
+
+q6_graph(100000, 4.98, -1.5e-3, True)
+
+
